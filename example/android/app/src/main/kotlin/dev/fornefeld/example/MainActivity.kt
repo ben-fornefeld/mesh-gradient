3fc9a594a3703a6d3b4affd8c8a857f33a4e97e9
Ben Fornefeld <ben.fornefeld@gmail.com>
2024-06-27 18:39:12 +0200

    :bug: fix late ticker not initialized

---
+++ example/android/app/src/main/kotlin/dev/fornefeld/example/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.fornefeld.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'package:mesh_gradient/mesh_gradient.dart';
 
